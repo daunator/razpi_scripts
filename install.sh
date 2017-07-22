13168a622503b6390aaa9a006b8de1344dc51529
Gabriel <gabi.francu@gmail.com>
2017-07-22 13:33:46 +0300

    Fix udev rule

--- install.sh
+++ install.sh
@@ -11,6 +11,6 @@ sudo bash -c 'echo "@reboot root raspi_gpio_actions_INT.py" >> /etc/crontab'
 sudo bash -c 'echo "@reboot root autodial_rds.sh" >> /etc/crontab'
 sudo bash -c 'echo "@reboot pi   /home/pi/bin/reverse_ssh.sh" >> /etc/crontab'
 
-sudo bash -c 'echo "SUBSYSTEMS==\"usb\", ATTR{idVendor}==\"12d1\", ATTR{idProduct}=\"15ca\", RUN+=\"/usr/bin/sg_raw /dev/sr0 11 06 20 00 00 00 00 00 01 00\"" >> /etc/udev/rules.d/10-HuaweiFlashCard.rules' 
+sudo bash -c 'echo "SUBSYSTEMS==\"usb\", ATTRS{idVendor}==\"12d1\", ATTRS{idProduct}==\"15ca\", RUN+=\"/usr/bin/sg_raw /dev/sr0 11 06 20 00 00 00 00 00 01 00\"" >> /etc/udev/rules.d/10-HuaweiFlashCard.rules' 
 
 echo "You need to edit \"reverse_ssh.sh\" and \"remote_shutdown.sh\" and replace x@x with the actual user@host of ssh server that acts as proxy."
